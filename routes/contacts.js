8a40f8ed1963d8d20bbdc1bf387f6f9437b469c6
Aabhimanyu Gupta <abhimanyu10gupta@gmail.com>
2022-02-01 00:03:45 +1000

    contact model and get contacts route

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
+const { check, validationResult } = require('express-validator/check');
+const auth = require('../middleware/auth');
+const Contact = require('../models/Contact');
 
 // @router      GET api/contacts
 // @desc        get all users contacts
 // @access      private
-router.get('/', (req, res) => {
-  res.send('Get all user contacts');
+router.get('/', auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1,
+    });
+
+    res.json(contacts);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ msg: 'Server Error' });
+  }
 });
 
 // @router      POST api/contacts
