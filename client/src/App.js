4aa70c9b3e8ea1f8e57c8485d97e5eb2c5efd290
Aabhimanyu Gupta <abhimanyu10gupta@gmail.com>
2022-02-10 21:31:07 +1000

    load user and set token

--- client/src/App.js
+++ client/src/App.js
@@ -6,22 +6,36 @@ import Home from './components/pages/Home';
 import About from './components/pages/About';
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
+import Register from './components/auth/register';
+import Login from './components/auth/login';
+import AlertState from './context/alert/AlertState';
+import Alerts from './components/layout/Alerts';
+import setAuthToken from './utils/setAuthToken';
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 const App = () => {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-          <Fragment>
-            <Navbar />
-            <div className='container'>
-              <Routes>
-                <Route exact path={'/'} element={<Home />} />
-                <Route exact path={'/about'} element={<About />} />
-              </Routes>
-            </div>
-          </Fragment>
-        </Router>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <Alerts />
+              <div className='container'>
+                <Routes>
+                  <Route exact path={'/'} element={<Home />} />
+                  <Route exact path={'/about'} element={<About />} />
+                  <Route exact path={'/register'} element={<Register />} />
+                  <Route exact path={'/login'} element={<Login />} />
+                </Routes>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthState>
   );
