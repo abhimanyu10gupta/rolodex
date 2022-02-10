08d2491774298f9d2742632b7938f7e01305d1b1
Aabhimanyu Gupta <abhimanyu10gupta@gmail.com>
2022-02-11 04:17:03 +1000

    update contacts

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -81,6 +81,33 @@ const ContactState = (props) => {
     }
   };
 
+  //update contact
+  const updateContact = async (contact) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    try {
+      const res = await axios.put(
+        `/api/contacts/${contact._id}`,
+        contact,
+        config
+      );
+
+      dispatch({
+        type: UPDATE_CONTACT,
+        payload: res.data,
+      });
+    } catch (err) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: err.response.data,
+      });
+    }
+  };
+
   // Clear Contacts
   const clearContacts = () => {
     dispatch({ type: CLEAR_CONTACTS });
@@ -94,10 +121,7 @@ const ContactState = (props) => {
   const clearCurrent = () => {
     dispatch({ type: CLEAR_CURRENT });
   };
-  //update contact
-  const updateContact = (contact) => {
-    dispatch({ type: UPDATE_CONTACT, payload: contact });
-  };
+
   //filter contacts
   const filterContacts = (text) => {
     dispatch({ type: FILTER_CONTACTS, payload: text });
